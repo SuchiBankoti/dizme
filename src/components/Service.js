7206ade8a4321245690ad49fd8ab806df632b516
suchi <suchibankoti95@gmail.com>
2024-03-17 11:16:06 +0530

    firts commit

---
+++ src/components/Service.js
@@ -0,0 +1,132 @@
+import parse from "html-react-parser";
+import { useEffect, useState } from "react";
+import { fatchData } from "../utilits";
+import ServicePopup from "./popup/ServicePopup";
+
+const Service = ({ dark }) => {
+  const [data, setData] = useState([]);
+  const [popupdata, setPopupdata] = useState({});
+  const [popup, setPopup] = useState(false);
+
+  useEffect(async () => {
+    // setData(await fatchData("/static/service.json"));
+    const serviceData = await fatchData(
+      "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae"
+    );
+    const services = serviceData.user.services;
+
+    const localData = await fatchData("/static/service.json");
+    const temp = localData.map((service, i) => {
+      const {
+        name: title,
+        charge: price,
+        desc: shortDesc,
+        image: { url: img },
+      } = services[i];
+      return {
+        title,
+        price,
+        shortDesc,
+        img,
+        icon: service.icon,
+        dec: service.dec,
+      };
+    });
+    setData(temp);
+    setTimeout(() => {
+      let VanillaTilt = require("vanilla-tilt");
+      VanillaTilt.init(document.querySelectorAll(".tilt-effect"), {
+        maxTilt: 6,
+        easing: "cubic-bezier(.03,.98,.52,.99)",
+        speed: 500,
+        transition: true,
+      });
+    }, 1000);
+  }, []);
+
+  const onClick = (index) => {
+    setPopup(true);
+    setPopupdata(data && data[index]);
+  };
+
+  return (
+    <div className="dizme_tm_section" id="service">
+      <ServicePopup
+        data={popupdata}
+        open={popup}
+        close={() => setPopup(false)}
+      />
+      <div className="dizme_tm_services">
+        <div className="container">
+          <div className="dizme_tm_main_title" serviceData-align="center">
+            <span>Services</span>
+            <h3>What I Do for Clients</h3>
+            <p>
+              Most common methods for designing websites that work well on
+              desktop is responsive and adaptive design
+            </p>
+          </div>
+          <div className="service_list">
+            <ul>
+              {data &&
+                data.map(
+                  (service, i) =>
+                    service && (
+                      <li
+                        className={`wow ${
+                          (i * 1) % 2 === 0 ? "fadeInLeft" : "fadeInRight"
+                        }`}
+                        obj-wow-duration="1s"
+                        key={i}
+                        onClick={() => onClick(i)}
+                      >
+                        <div className="list_inner tilt-effect">
+                          <span className="icon">
+                            {parse(service.icon.svg)}
+                            {dark ? (
+                              <img
+                                className="back"
+                                src={service.icon.iconBgDark}
+                                alt="image"
+                              />
+                            ) : (
+                              <img
+                                className="back"
+                                src={service.icon.iconBg}
+                                alt="image"
+                              />
+                            )}
+                          </span>
+                          <div className="title">
+                            <h3>{service.title}</h3>
+                            <span className="price">
+                              Starts from <span>{service.price}</span>
+                            </span>
+                          </div>
+                          <div className="text">
+                            <p>{service.shortDesc}</p>
+                          </div>
+                          <a className="dizme_tm_full_link" href="#" />
+                          <img
+                            className="popup_service_image"
+                            src="img/service/1.jpg"
+                            alt="image"
+                          />
+                        </div>
+                      </li>
+                    )
+                )}
+            </ul>
+          </div>
+        </div>
+        <div className="brush_1 wow fadeInLeft" serviceData-wow-duration="1s">
+          <img src="img/brushes/service/5.png" alt="image" />
+        </div>
+        <div className="brush_2 wow zoomIn" serviceData-wow-duration="1s">
+          <img src="img/brushes/service/6.png" alt="image" />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Service;
---
+++ src/components/Skills.js
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import { activeSkillProgress, fatchData } from "../utilits";
+import { INDEX_COLORS } from "../constants";
+
+const Skills = ({ dark }) => {
+  const [skills, setSkills] = useState({});
+  useEffect(async () => {
+    // setData(await fatchData("/static/info.json"));
+    const skillData = await fatchData(
+      "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae"
+    );
+    const skillArr = skillData.user.skills.map((obj, i) => {
+      const { percentage: value, name } = obj;
+      return {
+        value,
+        name,
+        color: INDEX_COLORS[i],
+      };
+    });
+    setSkills(skillArr);
+  }, []);
+  useEffect(() => {
+    window.addEventListener("scroll", activeSkillProgress);
+  }, []);
+
+  return (
+    <div className="dizme_tm_section">
+      <div className="dizme_tm_skills">
+        <div className="container">
+          <div className="wrapper">
+            <div className="left">
+              <div
+                className="dizme_tm_main_title wow fadeInUp"
+                data-wow-duration="1s"
+                data-align="left"
+              >
+                <span>Design is Life</span>
+                <h3>I Develop Skills Regularly to Keep Me Update</h3>
+                <p>
+                  Most common methods for designing websites that work well on
+                  desktop is responsive and adaptive design
+                </p>
+              </div>
+              <div
+                className="dodo_progress wow fadeInUp"
+                data-wow-duration="1s"
+              >
+                {skills &&
+                  skills.length &&
+                  skills.map((skill, i) => (
+                    <div
+                      className="progress_inner skillsInner___"
+                      data-value={skill.value}
+                      data-color={skill.color}
+                      key={i}
+                    >
+                      <span>
+                        <span className="label">{skill.name}</span>
+                        <span className="number">{skill.value}%</span>
+                      </span>
+                      <div className="background">
+                        <div className="bar">
+                          <div className="bar_in" />
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+              </div>
+            </div>
+            <div className="right">
+              <img src={`img/skills/${dark ? 2 : 1}.jpg`} alt="image" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Skills;
